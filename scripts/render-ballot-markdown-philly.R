b26612d214dd1e64394f0522d95403bfa254bfc3
maiarosenfeld <83026850+maiarosenfeld@users.noreply.github.com>
2024-10-30 19:00:43 -0400

    Update render-ballot-markdown-philly.R

--- scripts/render-ballot-markdown-philly.R
+++ scripts/render-ballot-markdown-philly.R
@@ -3,4 +3,4 @@ library(rmarkdown)
 # Render page
 rmarkdown::render('scripts/ballots-returned-tracker-philly.Rmd', 
                   output_dir = "docs",
-                  output_file = 'ballots-returned-tracker-philly.html')
+                  output_file = 'index.html')
